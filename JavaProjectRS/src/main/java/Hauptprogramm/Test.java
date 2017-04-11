ddb8a9a760979660c87a0a2e0f5132aa06e576ab
mfernitz <mfernitz@isdeblnnl065.iscout.local>
2017-04-11 16:34:22 +0200

    v0.3.0

--- JavaProjectRS/src/main/java/Hauptprogramm/Test.java
+++ JavaProjectRS/src/main/java/Hauptprogramm/Test.java
@@ -2,6 +2,7 @@ package Hauptprogramm;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
@@ -21,15 +22,13 @@ public class Test {
     @GET
     @Produces({MediaType.TEXT_HTML})
 	public FileInputStream testA() throws FileNotFoundException{
-   
 		File file = new File(cfp);
-		return new FileInputStream(file);
-
-    
+		return new FileInputStream(file); 
  }
     @POST
+    @Path("/einloggen")
     @Produces({MediaType.TEXT_HTML})
-    public FileInputStream testA(@FormParam("vn") String vorname ,@FormParam("nn") String nachname) throws FileNotFoundException{
+    public FileInputStream einloggen(@FormParam("vn") String vorname ,@FormParam("nn") String nachname) throws FileNotFoundException{
     	//uberpurefung des Namens kommt noch
     	boolean l = true;
     	if(l == false){
@@ -40,13 +39,38 @@ public class Test {
     	 File file = new File(cfp);
     	 return new FileInputStream(file);	
     	}
+    
  }
     @POST
     @Path("/WohnungAnlegen")
     public void testA(@FormParam("preis") String preis ,@FormParam("beschreibung") String beschreibung, @FormParam("groese") String groese, @FormParam("imagepfad") String imagepfad) throws FileNotFoundException{
    
     	Wohnung.wohnungAnlegen(katalog, preis, beschreibung, groese, imagepfad);
-    	testA("", "");
+    	einloggen("", "");
     
     }
+    @POST
+    @Path("/Buchen")
+    @Produces({MediaType.TEXT_HTML})
+    public String searchResults() throws FileNotFoundException{
+    	String html;
+        	 
+        	 html = "<!DOCTYPE html>\n<html>\n<head>\n<meta charset='UTF-8'><title>Insert title here</title></head><body><table border='1'>";
+    		for(int i = 0; i < katalog.length; i++){
+    			html += "<tr>";
+    			for (int x = 0; x < 4 ; x++){
+    				if(x ==3){
+    					html += "<td><button><img src="+katalog[i][x] +" width='190' height='108'></button></td>";
+    				}else{
+    					html += "<td>"+katalog[i][x] +"</td>";
+    				}
+    			}
+    			html += "</tr>";
+    		}
+    		html += "</table></body></html>";
+    		
+    	return html;
+    
+    }
+    
 }
